aa8d52c24bd833cb79e7fd2b2d1223cb797bb29d
Elizabeth Kipp <liz@elizabethkipp.com>
2015-06-23 22:29:32 -0500

    functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+ 
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,6 +12,30 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
+	var randNum;
+	var guessAmount = 0;
+	var guesses;
+
+
+	var hiddenRandNum = function() {
+		randNum = Math.ceil(Math.random()*100);
+		return randNum;
+	}
+
+  	var newGame=function() {
+  		$('#guessList li')
+  	}
+
+  	$('new').click(function() {
+  		newGame();
+  		console.log('Is this working?');
+
+
+  	})
+
+        });
+
+
+
 
 
